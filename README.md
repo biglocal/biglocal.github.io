8176577d7e07767e3ca2e4c171da74b32e031048
yu-an kuo <biglocal1025@gmail.com>
2020-04-21 11:18:03 +0800

    test mask map

---
+++ README.md
@@ -0,0 +1,74 @@
+Mask Map 口罩即時查
+===========================
+2020年初台灣口罩實施購買實名制，因而產生了民眾查詢各藥局口罩庫存的需求
+在政務委員唐鳳策劃下釋出了api, 以供民間自發行實做出各種查詢載體。
+
+****
+
+wenyo [口罩即時查](https://wenyo.github.io/maskmap/)  
+Penny Yang [UI設計稿](https://xd.adobe.com/view/6e126433-68ec-45ce-6c7d-62ac0cede399-1823/grid)  
+
+<img src="/img/readmeImg/maskmap.png" width="80%">
+
+# 操作說明
+## 搜尋藥局
+<img src="/img/readmeImg/searchByName.png" width="50%">
+
+- 可輸入關鍵字搜尋藥局
+
+## 搜尋地址
+<img src="/img/readmeImg/searchByLoaction.png" width="50%">
+
+- 預設進入頁面後，會請求定位搜尋五公里內藥局
+- 也可以輸入地區以藥局地址搜尋
+    >輸入「臺中市北區」，會列出地址在臺中市北區的藥局
+
+## 搜尋記錄
+<img src="/img/readmeImg/searchHistory.png" width="50%">
+
+- 搜尋藥局與搜尋地址皆會紀錄搜尋歷程於 localstorage 
+- 皆可按下「清除記錄」以清除
+
+## 篩選條件
+<img src="/img/readmeImg/dataFilter.png" width="40%">
+
+- 口罩種類
+  - 成人：僅顯示尚有成人口罩的藥局
+  - 兒童：僅顯示尚有兒童口罩的藥局
+  - 取消種類篩選：再次點擊已選取的條件即可
+   
+- 排序
+  - 距離 : 以定位位置為基準，由近至遠（有開啟定位的預設值）
+  - 數量 : 以數量多至少排序（無開啟定位的預設值）
+    - 選擇成人： 以成人數量排序
+    - 選擇兒童： 以兒童數量做排序
+    - 無口罩種類： 以成人+兒童數量做排序
+
+## 刷新列表
+<img src="/img/readmeImg/resetData.png" width="15%">
+
+- 可刷新即時口罩資訊與重新定位
+
+## 列表資訊
+<img src="/img/readmeImg/dataFrom.png" width="80%">
+
+<img src="/img/readmeImg/maskNum.png" width="35%">
+
+- 口罩數量：
+    - 大於100：綠色
+    - 0 ~ 100：橘色
+    - 0：灰色
+  
+<img src="/img/readmeImg/openStatus.png" width="35%">
+
+- 營業狀態：
+    - 營業中：綠色
+    - 即將休息：橘色
+    - 休息中：灰色
+
+****
+特別感謝
+
+kiang [口罩供需資訊平台](https://g0v.hackmd.io/gGrOI4_aTsmpoMfLP1OU4A)  
+[THE F2E 2nd 前端修煉精神時光屋](https://challenge.thef2e.com/)  
+
---
+++ all.js
@@ -0,0 +1,318 @@
+new Vue({
+    el: '#mask',
+    data: {
+        nowHour: new Date().getHours(), // 現在幾點
+        iWeeklyDay: new Date().getDay(), // 得到星期幾
+        vHours: [8, 12, 17, 22], // 上午 下午 晚上分界
+        iHour: -1, //vHours idx
+        vOpenClass: ['active', 'active_rest', 'rest'],
+        vOPenStr: ['營業中', '即將休息', '休息中'],
+        vCondition: { 'adult': false, 'childern': false },
+        sSort: 'number', // 預設以距離由近到遠排序
+
+        vDate: ['偶數', '單數', '大家'],
+        iDate: 0, // vDate idx
+        sUpdateTime: '', // 最新更新時間
+        vAllMaskData: [],
+        vShowMask: [],
+        vLoading: false,
+        dataNumberNow: 6,
+        dataNumber: 6,
+        vMaskImg: ['./img/ic_stock_full@2x.png', './img/ic_stock_few@2x.png', './img/ic_stock_none@2x.png'],
+        vMaskClass: ['full_mask', 'few_mask', 'none_mask'],
+        searchCity: '',
+        searchStore: '',
+        bTipShow: true,
+        bRule: false,
+        bWarning: false,
+        bSortShow: false,
+        bGetLocation: false,
+        iDistance: 5,
+        vHistory: [],
+        bFocus1: false,
+        bFocus2: false,
+        sMyLoction: '我的位置'
+    },
+    created() {
+        this.getIHour();
+        // this.getWeeklyDay();
+        this.searchCity = this.sMyLoction;
+        this.getLocalStorage();
+        this.getMaskData();
+    },
+    methods: {
+        // 得到口罩資料
+        getMaskData() {
+            this.vLoading = true;
+            this.vShowMask = [];
+            this.resetDataNum();
+            axios
+                .get('https://raw.githubusercontent.com/kiang/pharmacies/master/json/points.json', {})
+                .then(response => {
+                    this.vAllMaskData = response.data.features;
+                    for (const maskInfo of this.vAllMaskData) {
+                        let service_periods = maskInfo.properties.service_periods;
+                        maskInfo.properties.iAvailable = this.getOpenTime(service_periods);
+                    }
+                    this.sUpdateTime = this.vAllMaskData[1].properties.updated.substring(11);
+                    this.getLocation();
+                    this.vLoading = false;
+                })
+        },
+        // 得到購買者身份
+        getWeeklyDay() {
+            if (this.iWeeklyDay == 0) {
+                this.iDate = 2;
+            } else if (this.iWeeklyDay % 2 == 1) {
+                this.iDate = 1;
+            } else if (this.iWeeklyDay % 2 == 0) {
+                this.iDate = 0;
+            }
+        },
+        // 得到口罩庫存量的等級
+        getMaskStockIdx(maskNumber) {
+            let idx = (maskNumber > 100) ? 0 : (maskNumber !== 0 ? 1 : 2);
+            return idx;
+        },
+        // 得到營業時間 "N"=開診、"Y"=休診
+        getOpenTime(sOpenTime) {
+            // rest
+            if (this.iHour == -1) {
+                return 2;
+            }
+            let iOpenTimeIdx = (this.iWeeklyDay - 1) + this.iHour * 7;
+            let bOpenTime_now = sOpenTime[iOpenTimeIdx];
+            let bOpenTime_nextH = sOpenTime[iOpenTimeIdx + 1];
+            if (bOpenTime_now == 'Y') { // rest
+                return 2;
+            } else { // now open , to tell next hour
+                return bOpenTime_nextH == 'N' ? 0 : 1;
+            }
+        },
+        // 得到 iHour
+        getIHour() {
+            for (const keyH in this.vHours) {
+                if (this.nowHour - this.vHours[keyH] < 0) {
+                    this.iHour = keyH - 1;
+                    break;
+                }
+            }
+        },
+        // 過濾地區
+        getCity() {
+            if(this.searchCity !== this.sMyLoction){
+                for (const maskInfo of this.vAllMaskData) {
+                    let address = maskInfo.properties.address;
+                    if (this.searchCity == '') {
+                        this.vShowMask.push(maskInfo);
+                    } else if (address.indexOf(this.searchCity) > -1) {
+                        this.vShowMask.push(maskInfo);
+                    }
+                }
+            }else{
+                this.filterStore();
+            }
+            this.resetDataNum();
+            this.sortCondition();
+        },
+        // 過濾店家名稱
+        getStore() {
+            if(!this.searchStore) return;
+            this.vShowMask = this.vShowMask.filter(vStore => {
+                return vStore.properties.name.indexOf(this.searchStore) > -1;
+            })
+        },
+        // reset 數量
+        resetDataNum() {
+            this.dataNumberNow = this.dataNumber;
+        },
+        // 查看更多
+        getMoreData() {
+            this.dataNumberNow += this.dataNumber;
+        },
+        // 得到使用者所在位置
+        getLocation() {
+            this.bGetLocation = false;
+
+            if ( navigator.geolocation ) {
+                navigator.geolocation.getCurrentPosition(this.showLocation, this.notAllowLoction);
+            }else{
+                console.log('google location error');
+                this.getCity();
+            }
+        },
+        // 得到定位後
+        showLocation(vLocation) {
+            this.bGetLocation = true;
+            this.calDistance(vLocation);
+            this.sSort = 'distance';
+        },
+        // 不允許得到定位
+        notAllowLoction(){
+            this.searchCity = this.searchCity == this.sMyLoction ? '' : this.searchCity;
+            this.getCity();
+        },
+        // 從選單點選我的位置
+        clickMyLocation() {
+            if (this.bGetLocation) {
+                this.searchCity = this.sMyLoction;
+                this.getNearbyStore();
+            } else {
+                this.bWarning = true;
+            }
+        },
+        // 顯示距離內的藥局
+        filterStore() {
+            this.vShowMask = this.vAllMaskData.filter(vStore => {
+                return vStore.geometry.coordinates[2] <= this.iDistance;
+            })
+        },
+        // 過濾口罩數量
+        checkMaskNum(vShowMask) {
+            vShowMask = vShowMask.filter(vStore => {
+                let bAdult = !this.vCondition.adult || vStore.properties.mask_adult > 0;
+                let bChild = !this.vCondition.childern || vStore.properties.mask_child > 0;
+                return bAdult && bChild;
+            })
+            return vShowMask
+        },
+        // 計算距離
+        calDistance(vLocation) {
+            let userLongitude = vLocation.coords.longitude;
+            let userLatitude = vLocation.coords.latitude;
+            this.vAllMaskData.forEach(vStore => {
+                let storeLongitude = vStore.geometry.coordinates[0];
+                let storeLatitude = vStore.geometry.coordinates[1];
+                let a = storeLongitude - userLongitude;
+                let b = storeLatitude - userLatitude;
+                let distance = Math.sqrt(a * a + b * b) * 111;
+                vStore.geometry.coordinates[2] = distance;
+            });
+        },
+        // 由近到遠排序
+        sortStoreDistance() {
+            this.vShowMask.sort(function(x, y) {
+                return x.geometry.coordinates[2] - y.geometry.coordinates[2];
+            })
+        },
+        // 由多到少
+        sortStoreNum() {
+            if (this.vCondition.adult) {
+                this.vShowMask.sort(function(x, y) {
+                    return -x.properties.mask_adult + y.properties.mask_adult;
+                })
+            } else if (this.vCondition.childern) {
+                this.vShowMask.sort(function(x, y) {
+                    return -x.properties.mask_child + y.properties.mask_child;
+                })
+            } else {
+                this.vShowMask.sort(function(x, y) {
+                    return -(x.properties.mask_child + x.properties.mask_adult) + (y.properties.mask_child + y.properties.mask_adult);
+                })
+            }
+        },
+        // 排序選項
+        sortCondition() {
+            if (this.sSort == 'distance') {
+                this.sortStoreDistance();
+            } else if (this.sSort == 'number') {
+                this.sortStoreNum();
+            }
+        },
+        // 點擊以我的位置查詢
+        getNearbyStore() {
+            this.filterStore();
+            this.sortStoreDistance();
+        },
+        // 地圖 url
+        getStoreLocation(vMaskMap) {
+            let name = vMaskMap.properties.name
+            let address = vMaskMap.properties.address
+            return `https://www.google.com.tw/maps?q=${name} ${address}`;
+        },
+        // 增加搜尋歷史紀錄
+        addToLocalStorage(key, search) {
+            if (search == '' || search == this.sMyLoction) return;
+            this.vHistory[key].push(search);
+            this.vHistory[key] = Array.from(new Set(this.vHistory[key]));
+            this.saveToLocalStorage();
+        },
+        // 清除搜尋紀錄
+        clearLocalStorage(key) {
+            this.vHistory[key] = [];
+            this.saveToLocalStorage();
+        },
+        // 儲存 localStorage
+        saveToLocalStorage() {
+            sMaskData = JSON.stringify(this.vHistory);
+            localStorage.setItem('maskData', sMaskData)
+        },
+        // 將 localStorage 的記錄拿出來
+        getLocalStorage() {
+            if (localStorage.getItem('maskData') == null) {
+                let sMaskData = {
+                    'searchStore': [],
+                    'searchCity': []
+                };
+                sMaskData = JSON.stringify(sMaskData);
+                localStorage.setItem('maskData', sMaskData)
+            }
+            this.vHistory = JSON.parse(localStorage.getItem('maskData'));
+        },
+        //  判斷位置欄位
+        getDataFromLoction() {
+            if (this.searchCity == this.sMyLoction) {
+                this.getNearbyStore();
+            } else {
+                this.getCity();
+            }
+        },
+        filterIdentyfy(identyfy) {
+            this.vCondition[identyfy] = !this.vCondition[identyfy]
+        },
+        showLacationAlert() {
+            if (!this.bGetLocation) { this.bWarning = true }
+        },
+        scrollTip(){
+            const tipContent = this.$refs.tipContent;
+            tipContent.scrollBy(0, tipContent.scrollHeight);
+        },
+    },
+    watch: {
+        // 監聽搜尋地址
+        searchCity() {
+            let sSearchCity = this.searchCity;
+            setTimeout(function() {
+                if(sSearchCity !== this.searchCity) return;
+                this.vShowMask = [];
+                this.resetDataNum();
+                this.getDataFromLoction();
+                this.getStore();
+                this.addToLocalStorage('searchCity', this.searchCity);
+            }.bind(this), 1500)
+        },
+        // 監聽搜尋店家
+        searchStore() {
+            let sSearchStore = this.searchStore;
+
+            setTimeout(function() {
+                if(sSearchStore !== this.searchStore) return;
+                this.vShowMask = [];
+                this.resetDataNum();
+                this.getDataFromLoction();
+                this.getStore();
+                this.addToLocalStorage('searchStore', this.searchStore);
+            }.bind(this), 1500)
+        },
+        sSort() {
+            this.sortCondition();
+        },
+        bGetLocation(){
+            this.getDataFromLoction()
+            this.getStore();
+        },
+        vAllMaskData(){
+            this.vShowMask = [];
+        },
+    },
+})
